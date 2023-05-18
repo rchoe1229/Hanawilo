873651f3ed1e2f106e07fa47117a3e0d90e7d912
rchoe1229 <ryan.choe@outlook.com>
2023-05-18 12:07:29 -0500

    Added week 4 for review. Added supplementary array challenge problems.

---
+++ Array_Challenges/moveElementToEnd.js
@@ -0,0 +1,17 @@
+/**
+ * You're given an array of integers and an integer. Write a function that moves
+  all instances of that integer in the array to the end of the array and returns
+  the array.The function should perform this in place (i.e., it should mutate the input
+  array) and doesn't need to maintain the order of the other integers.
+ * 
+ * Example:
+ * Input:
+ * array = [2, 1, 2, 2, 2, 3, 4, 2]
+ * numberToMove = 2
+ * Output:
+ * [1, 3, 4, 2, 2, 2, 2, 2] 
+ */
+
+function moveElementToEnd(array, toMove) {
+  // Write your code here.
+}
