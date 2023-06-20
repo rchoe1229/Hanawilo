84c8c7aaf291f23b422dc2e385eb5098a90b2733
Ryan Choe <ryan.choe@outlook.com>
2023-06-19 18:17:39 -0600

    Update tournamentWinner.js

--- Week_3/tournamentWinner.js
+++ Week_3/tournamentWinner.js
@@ -9,6 +9,13 @@
   are no ties. A team receives 3 points if it wins and 0 points if it loses. The
   winner of the tournament is the team that receives the most amount of points.
 
+  Given an array of pairs representing the teams that have competed against each other and an array containing the results of each competition, write a function that returns the winner of the tournament.
+  The input arrays are named competitions and results, respectively. The competitions array has elements in the form of [homeTeam, awayTeam], where each team is a string of at most 30 characters representing the name of the team.
+  The results array contains information about the winner of each corresponding competition in the competitions array. 
+  Specifically, results[i] denotes the winner of competitions[i], where a 1 in the results array means that the home team in the corresponding competition won and a 0 means that the away team won.
+
+  It's guaranteed that exactly one team will win the tournament and that each team will compete against all other teams exactly once. It's also guaranteed that the tournament will always have at least two teams.
+
  * Example 1:
   * Inputs: 
     * competitions  = [
